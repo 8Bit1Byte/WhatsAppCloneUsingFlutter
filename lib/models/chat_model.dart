d3c645a8e253435e4e6a0082d092675fadfc9e5f
Exploore X <exploorex@gmail.com>
2021-06-18 17:45:23 +0530

    .

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,41 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Davinder Singh",
+      message: "Hey Flutter, is just new World!",
+      time: "15:30",
+      avatarUrl:
+          "https://images.generated.photos/NPf8riysq4OQljZGd27Q17l0NKKM7mqaFNpCnLQT2xM/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwNzA4NTUuanBn.jpg"),
+  new ChatModel(
+      name: "Angrej Brar",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "https://images.generated.photos/LP0tZtFaG_93fGuC5_ySEh4Yqc0q-hEIVSWpojBp6vM/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMjc0MDguanBn.jpg"),
+  new ChatModel(
+      name: "Amanat Kaur",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "https://images.generated.photos/Nb1rrL4HxQJBd4EsjTS3KCotaLIz0BgdSN_dyuct65A/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4ODg0NjcuanBn.jpg"),
+  new ChatModel(
+      name: "Sharan Brar",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "https://images.generated.photos/nQioa1d1GDWKVZJ0OEPiUmLCcQDZfsR4QcqGXe0HGck/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwODQ5NTIuanBn.jpg"),
+  new ChatModel(
+      name: "Raj Kaur",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://images.generated.photos/Nb1rrL4HxQJBd4EsjTS3KCotaLIz0BgdSN_dyuct65A/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4ODg0NjcuanBn.jpg"),
+];
---
+++ lib/pages/status_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutterwhatsapp/pages/store_page_view.dart';
+
+class StatusScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xfff2f2f2),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Card(
+            color: Colors.white,
+            elevation: 0.0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Stack(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                          "https://s3.amazonaws.com/wll-community-production/images/no-avatar.png"),
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      right: 1.0,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                title: Text(
+                  "My Status",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("Tap to add status update"),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Viewed updates",
+              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.white,
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                          "https://images.generated.photos/Nb1rrL4HxQJBd4EsjTS3KCotaLIz0BgdSN_dyuct65A/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4ODg0NjcuanBn.jpg"),
+                    ),
+                    title: Text(
+                      "Amanat Kaur",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("Today, 20:16 PM"),
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StoryPageView())),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
